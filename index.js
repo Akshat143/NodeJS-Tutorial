bb8a35cc3f44d096d76ebe76a9ed771c93b85408
Akshat143 <akshat.github@gmail.com>
2022-04-16 21:45:01 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 2000;
+const hostname = "localhost";
+const home = fs.readFileSync("./index.html", "utf-8");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    return res.end(home);
+  }
+  if (req.url === "/about") {
+    return res.end("<h1>About Page");
+  }
+  if (req.url === "/contact") {
+    return res.end("<h1>Contact Page");
+  }
+  if (req.url === "/service") {
+    return res.end("<h1>Service Page");
+  } else {
+    return res.end("<h1>404 Page not found!");
+  }
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server is working http://${hostname}:${PORT}`);
+});
